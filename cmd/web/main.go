7e142769e4b4e62dc2857099218c0a3ae8db0606
Diego López Torres <buyeye09@gmail.com>
2021-10-23 23:53:12 -0500

    Initial configuration
    Environment variables loading and server creation done

---
+++ cmd/web/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+)
+
+const PORT = ":4000"
+
+func main() {
+	var err error
+
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln("could not load environment variables", err.Error())
+		return
+	}
+
+	server := http.Server{
+		Addr: PORT,
+	}
+
+	fmt.Println("Server working on port", PORT)
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalln("could not initialize server", err.Error())
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/DieGopherLT/refaccionaria-backend
+
+go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
